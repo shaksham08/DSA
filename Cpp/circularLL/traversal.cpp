a7231244492d6187da95c662999c12747b2ba5bf
shaksham08 <shakshamsinha@outlook.com>
2020-07-10 17:58:43 +0530

    adding changes

---
+++ Cpp/circularLL/traversal.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+}
+
+main(int argc, char const *argv[])
+{
+    Node *head = new Node();
+
+    return 0;
+}
