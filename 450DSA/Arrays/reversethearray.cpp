efed9456a794bc4237123b48f3a8c101dba516b0
shaksham08 <shakshamsinha@outlook.com>
2021-04-08 13:31:30 +0530

    Adding reverse an array in 450DSA

---
+++ 450DSA/Arrays/reversethearray.cpp
@@ -0,0 +1,35 @@
+// Problem statement : Given an array(or string), the task is to reverse the array / string.
+// Examples:
+// Input : arr[] = {1, 2, 3} Output : arr[] = {3, 2, 1}
+// Input : arr[] = {4, 5, 1, 2} Output : arr[] = {2, 1, 5, 4}
+
+#include <iostream>
+
+void reverse(int arr[], int n)
+{
+    for (int i = 0; i < n / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    std::cout << "Enter the total number of items : ";
+    std::cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> arr[i];
+    }
+    reverse(arr, n);
+    std::cout << "The value stored in array after reversing it it : ";
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    return 0;
+}
---
+++ 450DSA/README.md
@@ -0,0 +1,3 @@
+# This Folder contains all the solved questions of 450 DSA questions given by Love Babbar.
+
+## [Link for the 450 DSA Problems](https://450dsa.com/)
