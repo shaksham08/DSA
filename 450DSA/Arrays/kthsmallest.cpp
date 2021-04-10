b0b80d735d6358f41c74efbfc0f013a2572cb76e
shaksham08 <shakshamsinha@outlook.com>
2021-04-10 23:45:04 +0530

    adding

--- 450DSA/Arrays/kthsmallest.cpp
+++ 450DSA/Arrays/kthsmallest.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 
+using namespace std;
+
+void minmax(int arr[], int n)
+{
+    int min = INT_MAX;
+    int max = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    std::cout << min << " " << max;
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int n;
+    std::cout << "Enter the total number of items : ";
+    std::cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> arr[i];
+    }
+    minmax(arr, n);
     return 0;
 }
