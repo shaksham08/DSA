a8a607a069a50d8a27b596a409ddf39e18ab6e71
shaksham08 <shakshamsinha@outlook.com>
2021-09-02 22:35:08 +0530

    Adding string tokenization program

--- Udemy/Strings/mytokenizer.cpp
+++ Udemy/Strings/mytokenizer.cpp
@@ -4,43 +4,41 @@ using namespace std;
 
 char *mystrtok(char *str, char delim)
 {
-    // static char *input = NULL;
+    static char *input = NULL;
 
-    // if (str != NULL)
-    // {
-    //     input = str;
-    // }
+    if (str != NULL)
+    {
+        input = str;
+    }
 
-    // if (input == NULL)
-    // {
-    //     return NULL;
-    // }
+    if (input == NULL)
+    {
+        return NULL;
+    }
 
-    // //static
-    // char *token = new char[strlen(input) + 1];
-    // int i = 0;
+    //static
+    char *token = new char[strlen(input) + 1];
+    int i = 0;
 
-    // for (; input[i] != '\0'; i++)
-    // {
-    //     if (input[i] != delim)
-    //     {
-    //         token[i] = input[i];
-    //     }
-    //     else
-    //     {
-    //         token[i] = '\0';
-    //         input = input + i + 1;
-    //         return token;
-    //     }
-    // }
+    for (; input[i] != '\0'; i++)
+    {
+        if (input[i] != delim)
+        {
+            token[i] = input[i];
+        }
+        else
+        {
+            token[i] = '\0';
+            input = input + i + 1;
+            return token;
+        }
+    }
 
-    // //out of the loop
-    // token[i] = '\0';
-    // //reset the input as nULL
-    // input = NULL;
-    // return token;
-    char *p = "hello world";
-    return p;
+    //out of the loop
+    token[i] = '\0';
+    //reset the input as nULL
+    input = NULL;
+    return token;
 }
 
 int main()
