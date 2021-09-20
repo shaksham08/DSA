c25b115ff1b32f8f0c16d63e1ee4c20fa0343e5a
shaksham08 <shakshamsinha@outlook.com>
2021-09-20 17:58:23 +0530

    adding program

---
+++ AV/DP/01_knapsack/07_minimumsubsetsumdiff.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int minsubsetsumdiff(int arr[], int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+    }
+
+    bool dp[n + 1][sum + 1];
+
+    for (int i = 0; i < n + 1; i++)
+    {
+        for (int j = 0; j < sum + 1; j++)
+        {
+            if (i == 0)
+            {
+                dp[i][j] = false;
+            }
+            if (j == 0)
+            {
+                dp[i][j] = true;
+            }
+        }
+    }
+
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j < sum + 1; j++)
+        {
+            if (arr[i] <= j)
+            {
+                dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    int minimum = INT_MAX;
+
+    for (int i = 0; i < (sum + 1) / 2; i++)
+    {
+        if (dp[n][i])
+            minimum = min(minimum, sum - (2 * i));
+    }
+    return minimum;
+}
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << minsubsetsumdiff(arr, n) << endl;
+    return 0;
+}
